a2d2a2b34b4f6ed6806018bd9ecd5dea80c95103
Vi-Stakhanov <stakhanov1987v@gmail.com>
2026-09-04 03:33:31 +0700

    верстка: первый коммит

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('.slider_intro_1').slick({
+        autoplay: false,
+        arrows: true, 
+        //fade: true,
+        //cssEase: 'linear',
+        dots: false,
+        appendArrows: $('#arrows-append_1') 
+    }); 
+          
+
+});
