d716f26752518272fe74debe6062f669e4444a83
Vi-Stakhanov <stakhanov1987v@gmail.com>
2026-09-07 23:58:56 +0700

    обновление верстки 2

--- js/common.js
+++ js/common.js
@@ -20,6 +20,29 @@ $(document).ready(function() {
         dots: false,
         appendArrows: $('#arrows-append_2') 
     }); 
+
+    $('.slider_gallery_1').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        infinite: true,
+        appendArrows: $('#arrows-append_3'),
+        responsive: [
+            {   
+              breakpoint: 1201,
+              settings: {
+                slidesToShow: 3
+              }
+            },  
+            {
+              breakpoint: 567,
+              settings: {
+                slidesToShow: 2
+              }
+            }   
+        ]
+    });
           
 
 });
